84061e83b1ffeb982682eaa1a5fdda5f71ef3813
Riccardo <riky.volto@live.com>
2023-09-26 16:35:24 +0200

    bug api

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,29 @@
 <script>
 import AppMain from './components/Main.vue'
 import AppHeader from './components/Header.vue'
-import axios from 'axios';
+import axios from 'axios'
 export default {
   name: 'App',
   data () {
     return {
-      
+      base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      card_sets: null,
+      card_images: null,
 
     }
   },
   components: {
     AppHeader,
     AppMain,
+
+  },
+  created() {
+    axios
+    .get(this.base_url)
+    .then(response => {
+      console.log(response.data.data.);
+      this.card_sets = response.data
+    })
   }
 }
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,15 @@
 <script>
+import {store} from '../store.js'
+
 
 export default {
   name: 'AppMain',
   data () {
     return {
-
+        store,
     }
   },
+
 
 }
 </script>
