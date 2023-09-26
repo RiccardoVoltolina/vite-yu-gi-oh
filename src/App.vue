447cf82ba56ffabed4eeffa6ff00bf6b9046e1d3
Riccardo <riky.volto@live.com>
2023-09-26 17:08:07 +0200

    fix bootstrap bug

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,11 @@
 <script>
 import AppMain from './components/Main.vue'
 import AppHeader from './components/Header.vue'
-import axios from 'axios'
 export default {
   name: 'App',
   data () {
     return {
-      base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
-      card_sets: null,
-      card_images: null,
+    
 
     }
   },
@@ -19,14 +16,7 @@ export default {
     AppMain,
 
   },
-  created() {
-    axios
-    .get(this.base_url)
-    .then(response => {
-      console.log(response.data.data[0].card_images[0].image_url);
-      this.card_sets = response.data.data
-    })
-  }
+
 }
 </script>
 
@@ -36,6 +26,6 @@ export default {
   
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  @use './assets/app'
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,7 @@
 <script>
 import {store} from '../store.js'
+import axios from 'axios'
+
 
 
 export default {
@@ -7,8 +9,19 @@ export default {
   data () {
     return {
         store,
+        base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+        card_sets: null,
+        card_images: null,
     }
   },
+  created() {
+    axios
+    .get(this.base_url)
+    .then(response => {
+      console.log(response.data.data[0].card_images[0].image_url);
+      this.card_sets = response.data.data
+    })
+  }
 
 
 }
