f8f0c1a5130f28cb558528a780960c515a7c79ba
Riccardo <riky.volto@live.com>
2023-09-26 17:38:42 +0200

    print first card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -20,6 +20,7 @@ export default {
     .then(response => {
       console.log(response.data.data[0].card_images[0].image_url);
       this.card_sets = response.data.data
+      console.log(this.card_sets);
     })
   }
 
@@ -28,8 +29,42 @@ export default {
 </script>
 
 <template>
+    <main class="bg_orange">
+        <div class="container w-75">
+            <div class="py-3">
+                <select class="form-select w-25" aria-label="Default select example">
+                    <option selected>Alien</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+            <div class="container bg-white p-5">
+                <h4 class="m-auto bg-black text-white py-2">Found 39 cards</h4>
+                <div class="bg_orange container g-0">
+                    <div class="row g-0">
+                        <div class="col-3">
+                            <img :src="card_sets[0].card_images[0].image_url" alt="">
+                        </div>
+                    </div>
+
+
+                </div>
+
+                
+
+
+            </div>
+
+        </div>
+
+    </main>
 </template>
 
 <style lang="scss" scoped>
+.bg_orange {
+    background-color: orange;
+    height: 600px;
+}
 
 </style>
